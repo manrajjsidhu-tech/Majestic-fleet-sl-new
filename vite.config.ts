495f39c5551a3c26173ced1909ac5c7f2ea22803
manrajjsidhu-tech <manrajj.sidhu@gmail.com>
2026-08-08 04:39:13 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(() => {
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GOOGLE_MAPS_PLATFORM_KEY': JSON.stringify(process.env.GOOGLE_MAPS_PLATFORM_KEY || '')
-    },
+    }
     resolve: {
       alias: {
         '@': path.resolve(process.cwd(), '.'),
